88dd9192add539686ca82d6fdaad263b7d8c5522
HwuanPage <gigolopage@gmail.com>
2025-06-18 17:11:00 +0900

    Book 저장 시 파일형식 Validation

--- Readio/src/main/java/ninegle/Readio/global/validation/EpubFile.kt
+++ Readio/src/main/java/ninegle/Readio/global/validation/EpubFile.kt
@@ -1,3 +1,14 @@
 package ninegle.Readio.global.validation
 
-annotation class EpubFile()
+import jakarta.validation.Constraint
+import jakarta.validation.Payload
+import kotlin.reflect.KClass
+
+@Target(AnnotationTarget.FIELD)
+@Retention(AnnotationRetention.RUNTIME)
+@Constraint(validatedBy = [EpubFileValidator::class])
+annotation class EpubFile(
+    val message: String = "EPUB 파일만 업로드할 수 있습니다.",
+    val groups: Array<KClass<*>> = [],
+    val payload: Array<KClass<out Payload>> = []
+)
--- Readio/src/main/java/ninegle/Readio/global/validation/EpubFileValidator.kt
+++ Readio/src/main/java/ninegle/Readio/global/validation/EpubFileValidator.kt
@@ -1,4 +1,11 @@
 package ninegle.Readio.global.validation
 
-class EpubFileValidator {
+import jakarta.validation.ConstraintValidator
+import jakarta.validation.ConstraintValidatorContext
+import org.springframework.web.multipart.MultipartFile
+
+class EpubFileValidator : ConstraintValidator<EpubFile, MultipartFile?> {
+    override fun isValid(file: MultipartFile?, context: ConstraintValidatorContext): Boolean {
+        return file == null || file.originalFilename?.lowercase()?.endsWith(".epub") == true
+    }
 }
